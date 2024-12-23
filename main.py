f2aaea5db3d43deb31afeb4a6433eac6597dc37b
Lazy Programmer <propigro@gmail.com>
2024-12-23 13:54:31 +0500

    2.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-for i in range(4):
-    rasm = cv2.imread(f"{i}.jpg")
+for i in range(5):
+    rasm = cv2.imread(f"rasm{i}.jpg")
     oqqora = cv2.cvtColor(rasm, cv2.COLOR_BGR2GRAY) 
-    cv2.imwrite(f"{i}_oqqora.jpg", oqqora)
+    cv2.imwrite(f"rasm{i}_oqqora.jpg", oqqora)
